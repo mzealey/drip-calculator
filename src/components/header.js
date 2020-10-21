f9d5c1df9752fdd6f9f2d970df50bed714e9456f
Mark Zealey <mark@markandruth.co.uk>
2020-10-21 12:32:21 +0100

    Remove boilerplate and put in basic work for joel

--- src/components/header.js
+++ src/components/header.js
@@ -30,7 +30,7 @@ export default class Header extends PureComponent {
         <nav className="navbar" role="navigation" aria-label="main navigation">
           <div className="container">
             <div className="navbar-brand">
-              <Link to="/" className=" navbar-item"><strong>ReactPWA</strong></Link>
+              <Link to="/" className=" navbar-item"><strong>Joels code</strong></Link>
               <button
                 type="button"
                 onClick={e => this.toggleMenuBar(e)}
@@ -49,34 +49,9 @@ export default class Header extends PureComponent {
               </button>
             </div>
             <div className={`navbar-menu ${open ? 'is-active' : ''}`}>
-              <Link className="navbar-item" to="/home" onClick={() => this.closeMenuBar()}>
-                Home
+              <Link className="navbar-item" to="/test-page" onClick={() => this.closeMenuBar()}>
+                Test page link
               </Link>
-              <Link className="navbar-item" to="/global-local-css" onClick={() => this.closeMenuBar()}>
-                Global & Local CSS
-              </Link>
-              <Link className="navbar-item" to="/typescript-counter" onClick={() => this.closeMenuBar()}>
-                TypeScript Counter
-              </Link>
-              <Link className="navbar-item" to="/skeleton-loading" onClick={() => this.closeMenuBar()}>
-                Skeleton Loading
-              </Link>
-              <Link className="navbar-item" to="/image-optimization" onClick={() => this.closeMenuBar()}>
-                Image Optimization
-              </Link>
-              <Link className="navbar-item" to="/login" onClick={() => this.closeMenuBar()}>
-                Auth
-              </Link>
-              <Link className="navbar-item" to="/contribute" onClick={() => this.closeMenuBar()}>
-                Contribute
-              </Link>
-              <a
-                className="navbar-item has-text-danger"
-                href="https://www.reactpwa.com"
-                onClick={() => this.closeMenuBar()}
-              >
-                Visit ReactPWA.com
-              </a>
             </div>
           </div>
         </nav>
